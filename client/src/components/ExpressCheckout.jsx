03ce8762671ae7073a31e100fcbefb77bdf6b1fe
Kristian Woods <kris_woods@hotmail.co.uk>
2021-04-20 20:13:21 +0100

    add styles module

--- client/src/components/ExpressCheckout.jsx
+++ client/src/components/ExpressCheckout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from '../styles/expressCheckout.module.css';
+
 
 function ExpressCheckout() {
   return (
-    <div>
-      <h4>ExpressCheckout</h4>
+    <div className={styles.Express}>
+      <h5>ExpressCheckout</h5>
+      <div className="payment-types-wrapper">
+        <button className="shopPay btn">shopPay</button>
+        <button className="googlePay muhbtn">googlePay</button>
+        <button className="payPal btn">payPal</button>
+      </div>
     </div>
   )
 }
