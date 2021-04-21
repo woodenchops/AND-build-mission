42950f72c1f75447305f3abf6ebdf7605139da91
Kristian Woods <kris_woods@hotmail.co.uk>
2021-04-21 08:26:06 +0100

    add logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Breadcrumb} from 'react-bootstrap';
+import { Breadcrumb } from 'react-bootstrap';
+import Logo from '../assets/images/tiger_head_sgrey.png';
 
 function NavBar() {
   return (
     <div>
-      <h4>Nav Bar</h4>
+      <h4>PAPER TIGER</h4>
+      <img src={Logo} alt='' />
       <Breadcrumb>
-        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/"> Library </Breadcrumb.Item>
+        <Breadcrumb.Item href='#'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item href='https://getbootstrap.com/docs/4.0/components/breadcrumb/'>
+          Library
+        </Breadcrumb.Item>
         <Breadcrumb.Item active>Data</Breadcrumb.Item>
       </Breadcrumb>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
