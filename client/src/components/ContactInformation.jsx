e2426129e68b1a251579e7145ebed4134edeb1c2
Kristian Woods <kris_woods@hotmail.co.uk>
2021-04-21 08:22:40 +0100

    set up prettier and eslint

--- client/src/components/ContactInformation.jsx
+++ client/src/components/ContactInformation.jsx
@@ -2,34 +2,37 @@ import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function ContactInformation() {
+  const x = 'hghg';
   return (
     <div>
       <h4>ContactInformation</h4>
 
-
       <h>Contact Information - Already have an account? Log in</h>
-          <Form>
-              <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                  </Form.Text>
-              </Form.Group>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Text className='text-muted'></Form.Text>
+        </Form.Group>
 
-              <Form.Group controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Keep me up to date on news and exclusive offers" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Sign up for loyalty scheme and create account" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                  Submit
-              </Button>
-        </Form>
-
-
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check
+            type='checkbox'
+            label='Keep me up to date on news and exclusive offers'
+          />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check
+            type='checkbox'
+            label='Sign up for loyalty scheme and create account'
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default ContactInformation
+export default ContactInformation;
