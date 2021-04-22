47d56601dfe5c423bed2da980e03a5b942a7a5d0
Corrie McGregor <corriemcgregor@corries-mbp.home>
2021-04-22 15:56:31 +0100

    Order summary and top text completed

---
+++ client/src/components/TopPart.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import OrderSummary from './OrderSummary';
+import { BasketContext } from '../context/basketContext';
+
+function TopPart() {
+  const { basket } = useContext(BasketContext);
+
+  return (
+    <section className='top-part section-container'>
+      <div>
+        <h1>THANK YOU!</h1>
+        <h5>
+          Your order has now been placed and you've successfully registered with
+          our new loyalty scheme.
+        </h5>
+        <hr />
+        <OrderSummary basket={basket} />
+        <p>
+          Please see your <a href='#'>Tracking Information</a>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default TopPart;
