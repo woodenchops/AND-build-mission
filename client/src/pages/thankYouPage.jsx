6e3055f929168b0a1427ffcc86fb2890ab162f48
Corrie McGregor <corriemcgregor@corries-mbp.home>
2021-04-22 14:09:07 +0100

    new branch for thank you page

--- client/src/pages/thankYouPage.jsx
+++ client/src/pages/thankYouPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import React, { useContext } from 'react';
+import CheckoutSummary from './CheckoutSummary';
+import { BasketContext } from '../context/basketContext';
 
 function thankYouPage() {
-  return <div></div>;
+  const { basket } = useContext(BasketContext);
+
+  return (
+    <section className='thank-you section-container'>
+      <div>
+        <CheckoutSummary basket={basket} />
+      </div>
+    </section>
+  );
 }
 
 export default thankYouPage;
