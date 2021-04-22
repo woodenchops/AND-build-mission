eee1508714bef07dd936cd8a02d121969adfac5e
Kristian Woods <kris_woods@hotmail.co.uk>
2021-04-22 16:42:34 +0100

    add logo

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PTLogo from '../assets/images/tiger_head_sgrey.png';
+
+function Logo() {
+  return (
+    <div>
+      <h4>PAPER TIGER</h4>
+      <img src={PTLogo} alt='' />
+    </div>
+  );
+}
+
+export default Logo;
--- client/src/pages/loyaltyDisplayPage.jsx
+++ client/src/pages/loyaltyDisplayPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../components/Logo';
 
 function LoyaltyDisplayPage() {
   return (
     <div>
-      <h4>test</h4>
+      <Logo />
     </div>
   );
 }
