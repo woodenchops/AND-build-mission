1115714ded6c900f1ae2b0e7e667696c6173b0bc
Corrie McGregor <corriemcgregor@corries-mbp.home>
2021-04-22 14:57:44 +0100

    remove basket provider line

--- client/src/pages/checkoutPage.jsx
+++ client/src/pages/checkoutPage.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import LeftSide from '../components/LeftSide';
 import RightSide from '../components/RightSide';
-import { BasketProvider } from '../context/basketContext';
 
 function CheckoutPage() {
   return (
     <main className='main-wrapper'>
       <LeftSide />
-      <BasketProvider>
-        <RightSide />
-      </BasketProvider>
+      <RightSide />
     </main>
   );
 }
