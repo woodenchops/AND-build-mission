f6eac120b11b60321fb2b5c04807a118c69807b9
Ross Cumming <rosso5792@gmail.com>
2021-04-23 10:33:35 +0100

    started on loyalty page left side

---
+++ client/src/components/LoyaltyDisplayPageLeftSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Image, Row, Col } from 'react-bootstrap';
+import Loyalty from '../assets/images/Loyalty.png';
+
+function LoyaltyDisplayPageLeftSide() {
+  return (
+    <section>
+      <Row noGutter></Row>
+    </section>
+  );
+}
+
+export default LoyaltyDisplayPageLeftSide;
